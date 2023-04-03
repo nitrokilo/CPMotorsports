d8d48b8acce3a8844c7391f51e013a18cb4675e7
LukeG-dev <luke.gilin@gmail.com>
2023-04-02 23:33:48 -0500

    restore completed projects

---
+++ CpmotorsportsApp/src/scenes/reports/completed_projects_per_customer.jsx
@@ -0,0 +1,104 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { useEffect, useState } from "react";
+import client from "../../Api/apiconfig.js";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+import MaterialTable from "material-table";
+import { tableIcons } from "../global/tableicons";
+const Completed_projects = () => {
+  // State intialization for rerender to control page render
+  const [reRender, setReRender] = useState(false);
+  // Api Call and config
+  const [Data, setData] = useState([]);
+  useEffect(() => {
+    client
+      .get("/reports/completed_projects_per_customer")
+      .then((res) => {
+        setData(res.data);
+      })
+      .then(setReRender(false))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [reRender]);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const currencyFormatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  // Column Configuration
+  const columns = [
+    { field: "Customer", title: "Customer", flex: 0.5 },
+    { field: "Most Recent Job", title: "Most Recent Job", flex: 1, cellClassName: "name-column--cell",},
+    { field: "Num of Projects", title: "Num of Projects", flex: 1 },
+   
+  ];
+  return (
+    <Box m="20px">
+      <Header title="Completed Projects" subtitle="List of all Completed Projects" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <MaterialTable
+          icons={tableIcons}
+          title="Completed Projects Per Customer"
+          data={Data}
+          columns={columns}
+          options={{
+            headerStyle: {
+              backgroundColor: "white",
+              color: "black",
+            },
+            actionsColumnIndex: -1,
+            addRowPosition: "first",
+            exportMenu: [
+              {
+                label: "Export PDF",
+                exportFunc: (cols, datas) => ExportPdf(cols, datas, "Completed_Projects_Per_Year"),
+              },
+              {
+                label: "Export CSV",
+                exportFunc: (cols, datas) => ExportCsv(cols, datas, "Completed_Projects_Per_Year"),
+              },
+            ],
+            filtering: true,
+            pageSize: 15,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+export default Completed_projects;
